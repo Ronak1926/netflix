7e828e8c639da0713c0b4316687e15d7b200de55
Ronak <ronaksojitra1926@gmail.com>
2025-09-04 18:30:47 +0530

    search history is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import SignupPage from './page/SignupPage'
 import WatchPage from './page/WatchPage'
 import Footer from './components/Footer'
 import SearchPage from './page/SearchPage'
+import SearchHistoryPage from './page/SearchHistoryPage'
 
 import { Toaster } from 'react-hot-toast'
 import { useAuthStore } from './store/authUser'
@@ -37,6 +38,7 @@ function App() {
         <Route path='/signup' element={!user ? <SignupPage /> : <Navigate to={"/"} />} />
         <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
         <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
       </Routes>
       <Footer />
       <Toaster />
